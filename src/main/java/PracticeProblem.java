b7816cdd823cf4ed862aa1fa891bf0e34ac59943
kyler-huang-ics4u1-01-25 <kyler.huang@student.tdsb.on.ca>
2025-11-01 19:23:42 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,21 @@
-public class PracticeProblem {
+class PracticeProblem {
+    public static void main(String[] args) {
+        char[] letters = {'d', 'a', 'c', 'b'};
+        insertionSort(letters);
+        for (int i = 0; i < letters.length; i++) {
+            System.out.print(letters[i] + " ");
+        }
+    }
 
-	public static void main(String args[]) {
-
-	}
-
-
+    public static void insertionSort(char[] characters) {
+        for (int i = 1; i < characters.length; i++) {
+            char key = characters[i];
+            int j = i - 1;
+            while (j >= 0 && characters[j] > key) {
+                characters[j + 1] = characters[j];
+                j--;
+            }
+            characters[j + 1] = key;
+        }
+    }
 }
